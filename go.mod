011acad3a4479b3ac6a03f240760960174d86d16
adionmb <adionmb@gmail.com>
2021-05-14 00:10:23 +0800

    simple app

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gin_example
 
 go 1.16
+
+require github.com/gin-gonic/gin v1.7.1 // indirect
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main()  {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
+	r.Run()
+}
